632d7b68a886d60b6d359dd876ee3985bea18350
Faruk Eryilmaz <frkerylmz@gmail.com>
2022-10-05 23:07:32 +0300

    Initial commit

---
+++ src/main/java/com/project/roadmap/ProjectRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.project.roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRoadmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRoadmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/roadmap/ProjectRoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.roadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRoadmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
