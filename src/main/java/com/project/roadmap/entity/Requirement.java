f8ff7390bd6b20677649460f6f69ad7511cb2359
Muhammet Ali Taban <m.ali.tabann28765@gmail.com>
2022-10-14 10:10:02 +0300

    feat: Requirement sınıfına yeni metodlar eklenerek gereksinimlere linklenmiş taskların kaçta kaçının bittiği bilgisi roadmap.html üzerinde görselleştirildi.

--- src/main/java/com/project/roadmap/entity/Requirement.java
+++ src/main/java/com/project/roadmap/entity/Requirement.java
@@ -1,20 +1,21 @@
 package com.project.roadmap.entity;
 
 import java.util.List;
+
 import com.project.roadmap.entity.Constants.RequirementState;
 
 public class Requirement {
 
     private String requirementTitle;
-
     private RequirementState requirementState;
-
     private List<Task> taskList;
+    private int closedTaskCount;
 
-    public Requirement(String requirementTitle, List<Task> taskList,RequirementState requirementState) {
+    public Requirement(String requirementTitle, List<Task> taskList, RequirementState requirementState) {
         this.requirementTitle = requirementTitle;
         this.taskList = taskList;
         this.requirementState = requirementState;
+        setClosedTaskCount();
     }
 
     public String getRequirementTitle() {
@@ -27,6 +28,26 @@ public class Requirement {
 
     public RequirementState getRequirementState() {
         return requirementState;
+    }
+
+    public int getClosedTaskCount() {
+        return closedTaskCount;
+    }
+
+    public void setClosedTaskCount() {
+        closedTaskCount = 0;
+        for (Task task : taskList) {
+            if (task.getTaskState() == Constants.TaskState.CLOSED)
+                closedTaskCount++;
+        }
+    }
+
+    public int getTaskCount() {
+        return taskList.size();
+    }
+
+    public String getClosedTaskCountPercent(){
+        return " ["+getClosedTaskCount()+"/"+getTaskCount()+"]  %"+ Math.round(((float)getClosedTaskCount()/getTaskCount()) *100);
     }
 
 }
