5ceedccfdf22ff79781e759988d0af465893af1f
Muhammet Ali Taban <m.ali.tabann28765@gmail.com>
2022-10-14 10:01:41 +0300

    feat: GitLabApiService içerisindeki getProjectRoadmapStatus() metodu tekrardan düzenlendi; FactoryEntity sınıfındaki getMilestoneInstance() metodu Mileston'nun durumuna göre instance döndürecek şekilde tekrardan düzenlendi.

--- src/main/java/com/project/roadmap/entity/FactoryEntity.java
+++ src/main/java/com/project/roadmap/entity/FactoryEntity.java
@@ -2,31 +2,43 @@ package com.project.roadmap.entity;
 
 import java.util.List;
 import org.gitlab4j.api.Constants.IssueState;
+
 public class FactoryEntity {
 
-    public static Milestone getMilestoneInstance(String milestoneTitle, List<Requirement> requirementList){
-        return new Milestone(milestoneTitle,requirementList);
+    public static Milestone getMilestoneInstance(String milestoneTitle, List<Requirement> requirementList, String milestoneState) {
+        if (milestoneState.equals("closed")) {
+            //Milestone altındaki Requirement'ların durumlarını kontrol ediyoruz
+            for (Requirement requirement : requirementList) {
+                // Requirement OPENED durumundaysa geriye NOT_CLOSED_YET durumunda Milestone nesnesi dönüyoruz
+                if (requirement.getRequirementState() == Constants.RequirementState.OPENED)
+                    return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.NOT_CLOSED_YET);
+            }
+            // Bütün Requirement'lar CLOSED durumundaysa
+            return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.CLOSED);
+        } else
+            //Milestone OPENED durumdaysa
+            return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.OPENED);
     }
 
-    public static Requirement getRequirementInstance(String requirementTitle, List<Task> taskList, IssueState requirementState){
+    public static Requirement getRequirementInstance(String requirementTitle, List<Task> taskList, IssueState requirementState) {
 
         if (requirementState == IssueState.CLOSED) {
             //Requirement altındaki Task'ların durumlarını kontrol ediyoruz
             for (Task task : taskList) {
-                // Task OPENED durumundaysa geriye NOT_CLOSED_YET durumunu dönüyoruz
+                // Task OPENED durumundaysa geriye NOT_CLOSED_YET durumunda Requirement nesnesi dönüyoruz
                 if (task.getTaskState() == Constants.TaskState.OPENED)
-                    return new Requirement(requirementTitle,taskList, Constants.RequirementState.NOT_CLOSED_YET);
+                    return new Requirement(requirementTitle, taskList, Constants.RequirementState.NOT_CLOSED_YET);
             }
             // Bütün tasklar CLOSED durumundaysa
-            return new Requirement(requirementTitle,taskList, Constants.RequirementState.CLOSED);
+            return new Requirement(requirementTitle, taskList, Constants.RequirementState.CLOSED);
         } else
             // Requirement OPENED durumundaysa
-        return new Requirement(requirementTitle,taskList, Constants.RequirementState.OPENED);
+            return new Requirement(requirementTitle, taskList, Constants.RequirementState.OPENED);
 
     }
 
-    public static Task getTaskInstance(String taskTitle,IssueState taskState){
-        if(taskState == IssueState.CLOSED){
+    public static Task getTaskInstance(String taskTitle, IssueState taskState) {
+        if (taskState == IssueState.CLOSED) {
             return new Task(taskTitle, Constants.TaskState.CLOSED);
         } else {
             return new Task(taskTitle, Constants.TaskState.OPENED);
