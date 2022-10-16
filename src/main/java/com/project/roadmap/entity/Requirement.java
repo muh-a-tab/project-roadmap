d59008e580701a4032a6187c07114f4d77a9f39d
Muhammet Ali Taban <m.ali.tabann28765@gmail.com>
2022-10-16 14:12:47 +0000

    feat: Milestone'ların durumuna göre listelenmesi ve roadmap.html üzerinde gösterimi. #15

--- src/main/java/com/project/roadmap/entity/Requirement.java
+++ src/main/java/com/project/roadmap/entity/Requirement.java
@@ -1,17 +1,16 @@
 package com.project.roadmap.entity;
 
 import java.util.List;
+
 import com.project.roadmap.entity.Constants.RequirementState;
 
 public class Requirement {
 
     private String requirementTitle;
-
     private RequirementState requirementState;
-
     private List<Task> taskList;
 
-    public Requirement(String requirementTitle, List<Task> taskList,RequirementState requirementState) {
+    public Requirement(String requirementTitle, List<Task> taskList, RequirementState requirementState) {
         this.requirementTitle = requirementTitle;
         this.taskList = taskList;
         this.requirementState = requirementState;
