f3f60290e74fd24f6ad2b668c27d2753820b8a45
Faruk Eryilmaz <frkerylmz@gmail.com>
2022-10-17 06:37:02 +0000

    feat: Completion info of milestones and requirements on Gantt Chart #16 #18 #19

--- src/main/java/com/project/roadmap/entity/FactoryEntity.java
+++ src/main/java/com/project/roadmap/entity/FactoryEntity.java
@@ -1,40 +1,41 @@
 package com.project.roadmap.entity;
 
+import java.util.Date;
 import java.util.List;
 import org.gitlab4j.api.Constants.IssueState;
 
 public class FactoryEntity {
 
-    public static Milestone getMilestoneInstance(String milestoneTitle, List<Requirement> requirementList, String milestoneState) {
+    public static Milestone getMilestoneInstance(Long id, String milestoneTitle, List<Requirement> requirementList, String milestoneState,
+                                                 Date startDate, Date endDate) {
         if (milestoneState.equalsIgnoreCase("closed")) {
             //Milestone altındaki Requirement'ların durumlarını kontrol ediyoruz
             for (Requirement requirement : requirementList) {
                 // Requirement OPENED durumundaysa geriye NOT_CLOSED_YET durumunda Milestone nesnesi dönüyoruz
                 if (requirement.getRequirementState() != Constants.RequirementState.CLOSED)
-                    return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.NOT_CLOSED_YET);
+                    return new Milestone(id, milestoneTitle, requirementList, Constants.MilestoneState.NOT_CLOSED_YET, startDate, endDate);
             }
             // Bütün Requirement'lar CLOSED durumundaysa
-            return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.CLOSED);
+            return new Milestone(id, milestoneTitle, requirementList, Constants.MilestoneState.CLOSED, startDate, endDate);
         } else
             //Milestone OPENED durumdaysa
-            return new Milestone(milestoneTitle, requirementList, Constants.MilestoneState.OPENED);
+            return new Milestone(id, milestoneTitle, requirementList, Constants.MilestoneState.OPENED, startDate, endDate);
     }
 
-    public static Requirement getRequirementInstance(String requirementTitle, List<Task> taskList, IssueState requirementState) {
-
+    public static Requirement getRequirementInstance(Long id, String requirementTitle, List<Task> taskList, IssueState requirementState,
+                                                     Date startDate, Date endDate) {
         if (requirementState == IssueState.CLOSED) {
             //Requirement altındaki Task'ların durumlarını kontrol ediyoruz
             for (Task task : taskList) {
                 // Task OPENED durumundaysa geriye NOT_CLOSED_YET durumunda Requirement nesnesi dönüyoruz
                 if (task.getTaskState() != Constants.TaskState.CLOSED)
-                    return new Requirement(requirementTitle, taskList, Constants.RequirementState.NOT_CLOSED_YET);
+                    return new Requirement(id, requirementTitle, taskList, Constants.RequirementState.NOT_CLOSED_YET, startDate, endDate);
             }
             // Bütün tasklar CLOSED durumundaysa
-            return new Requirement(requirementTitle, taskList, Constants.RequirementState.CLOSED);
+            return new Requirement(id, requirementTitle, taskList, Constants.RequirementState.CLOSED, startDate, endDate);
         } else
             // Requirement OPENED durumundaysa
-            return new Requirement(requirementTitle, taskList, Constants.RequirementState.OPENED);
-
+            return new Requirement(id, requirementTitle, taskList, Constants.RequirementState.OPENED, startDate, endDate);
     }
 
     public static Task getTaskInstance(String taskTitle, IssueState taskState) {
--- src/main/java/com/project/roadmap/entity/Milestone.java
+++ src/main/java/com/project/roadmap/entity/Milestone.java
@@ -1,19 +1,28 @@
 package com.project.roadmap.entity;
 
+import java.util.Date;
 import java.util.List;
 import com.project.roadmap.entity.Constants.MilestoneState;
 
-public class Milestone {
+public class Milestone implements Comparable<Milestone> {
+    private final Long id;
+    private final String milestoneTitle;
+    private final MilestoneState milestoneState;
+    private final List<Requirement> requirementList;
+    private final Date startDate;
+    private final Date endDate;
 
-    private String milestoneTitle;
-    private MilestoneState milestoneState;
-    private List<Requirement> requirementList;
-
-    public Milestone(String milestoneTitle, List<Requirement> requirementList, MilestoneState milestoneState) {
+    public Milestone(Long id, String milestoneTitle, List<Requirement> requirementList, MilestoneState milestoneState,
+                     Date startDate, Date endDate) {
+        this.id = id;
         this.milestoneTitle = milestoneTitle;
         this.requirementList = requirementList;
         this.milestoneState = milestoneState;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
+
+    public Long getId() { return id; }
 
     public String getMilestoneTitle() {
         return milestoneTitle;
@@ -25,5 +34,14 @@ public class Milestone {
 
     public MilestoneState getMilestoneState() {
         return milestoneState;
+    }
+
+    public Date getStartDate() { return startDate; }
+
+    public Date getEndDate() {return endDate; }
+
+    @Override
+    public int compareTo(Milestone o) {
+        return getStartDate().compareTo(o.getStartDate());
     }
 }
